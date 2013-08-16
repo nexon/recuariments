1741e2154c8394bbb528ec41127debd0e50eb898
nexon <alberto.lagos@gmail.com>
2013-08-16 15:27:21 -0400

    - Remove @projects ivar create in application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :current_user_projects
-  
-  
-  def current_user_projects
-    @projects = current_user.projects
-  end
+  before_action :authenticate_user!
 end
