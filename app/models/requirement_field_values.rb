98aa47dd9ed4d8c66bbda5db3000340f7fcca3be
nexon <alberto.lagos@gmail.com>
2013-08-17 11:59:34 -0400

    - Support for a field_value to be required if the checkbox of "required" is true

--- app/models/requirement_field_values.rb
+++ app/models/requirement_field_values.rb
@@ -2,5 +2,7 @@ class RequirementFieldValues < ActiveRecord::Base
   belongs_to :requirement
   belongs_to :requirement_field
   
+  validates_presence_of  :value, if: -> (field_value) {field_value.requirement_field.required == true}
+  
   delegate   :field_type, :name, :field_name, to: :requirement_field
 end
