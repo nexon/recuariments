6c47a0b6dfe337e5ce670ff2b53c2d98cf8a2f8d
nexon <alberto.lagos@gmail.com>
2013-08-17 14:10:28 -0400

    - Added text area partial for render in field creation - now the field (in partials) are an array.

--- app/models/requirement_field.rb
+++ app/models/requirement_field.rb
@@ -1,7 +1,7 @@
 class RequirementField < ActiveRecord::Base
   belongs_to :project
   
-  SUPPORTED_FIELD_TYPE = {"text_field" => "string", "check_box" => "boolean"}
+  SUPPORTED_FIELD_TYPE = %w(text_field check_box textarea)
   
   before_validation :parameterize_name
   validates_uniqueness_of :field_name, scope: [:project_id]
