a02fec57315c798ea171f90c1e6e3cacd98ad0d4
nexon <alberto.lagos@gmail.com>
2013-08-17 01:16:55 -0400

    - Remove custom validator in order to use validates_uniqueness_of. pretty neat!

--- config/initializers/uniqueness_field_name.rb
+++
@@ -1,7 +0,0 @@
-class UniquenessFieldName < ActiveModel::Validator
-  def validate(record)
-    unless record.project.fields.find_by_field_name(record.name.parameterize.gsub("-","_")).blank?
-      record.errors[:base] << "name can't be repeated"
-    end
-  end
-end
