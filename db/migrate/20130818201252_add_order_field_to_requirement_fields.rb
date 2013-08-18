d674b9c257ed61b2bf6eb17111b6a2584587f550
nexon <alberto.lagos@gmail.com>
2013-08-18 17:39:06 -0400

    - Change default value for order field in migration (99999 => 0) due the new implementation for ordering.

--- db/migrate/20130818201252_add_order_field_to_requirement_fields.rb
+++ db/migrate/20130818201252_add_order_field_to_requirement_fields.rb
@@ -1,5 +1,5 @@
 class AddOrderFieldToRequirementFields < ActiveRecord::Migration
   def change
-    add_column :requirement_fields, :order, :integer, default: 99999
+    add_column :requirement_fields, :order, :integer, default: 0
   end
 end
