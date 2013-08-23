3d86713244634ddf498de21d276aacd8107241bd
nexon <alberto.lagos@gmail.com>
2013-08-22 23:25:38 -0400

    - Added css files from "Awesome Admin"

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,2 +1,19 @@
 class CollaboratorsController < ApplicationController
+  def new
+    begin
+      @project = current_user.projects.find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to project_path(@project), alert: "Project not found."
+    else
+      @collaborator = @project.users.build
+    end
+  end
+  
+  def create
+    if false
+      "OLI"
+    else
+      render :new
+    end
+  end
 end
