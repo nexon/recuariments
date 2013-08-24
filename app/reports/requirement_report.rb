9968c4e4f04b0b4c01f93a6ca354c8c30a0b22ce
nexon <alberto.lagos@gmail.com>
2013-08-23 21:36:32 -0400

    - Now if a requirement has a empty attribute (with no value) don't matter because it will appear anyway in the pdf (when exporting to pdf)

--- app/reports/requirement_report.rb
+++ app/reports/requirement_report.rb
@@ -5,11 +5,13 @@ class RequirementReport < Prawn::Document
   def to_pdf(records)
     # text "Hello world"    
     
+    fields = records.first.project.fields
     records.each do |requirement|
       rows = []
-      requirement.requirement_attributes.reverse.each do |attribute|
-        rows << [Prawn::Table::Cell::Text.new(self, [0,0], content: attribute.requirement_field.name,
-                                              inline_format:  true,background_color:  'E8E8D0', font: "Helvetica", font_style: :bold), attribute.value]
+      fields.each do |attribute|
+        value = requirement.send(attribute.field_name)
+        rows << [Prawn::Table::Cell::Text.new(self, [0,0], content: attribute.name,
+                                              inline_format:  true,background_color:  'E8E8D0', font: "Helvetica", font_style: :bold), value.blank? ? "" : value ]
       end
       table(rows)
       move_down 20
