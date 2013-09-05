bfa6ff40766031a3b04b790867f87291bada1be3
Alberto Lagos Toro <alberto.lagos@gmail.com>
2013-09-05 16:26:16 -0400

    - Update models to be dependent with other models.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@ class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
   
-  has_many :memberships, inverse_of: :project
+  has_many :memberships, inverse_of: :project, dependent: :destroy
   has_many :users, through: :memberships
-  has_many :messages
+  has_many :messages, dependent: :destroy
   
   # belongs_to :owner, class_name: "User"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  	devise :database_authenticatable, :registerable,#,:confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,#,:confirmable,
+       :recoverable, :rememberable, :trackable, :validatable
+       
+  has_many :memberships, dependent: :destroy
+  has_many :projects, through: :memberships
 
-        has_many :memberships
-        has_many :projects, through: :memberships
-         
-         # has_many :projects, dependent: :destroy
-         
-   def self.create_user(email)
-     generated_password = Devise.friendly_token.first(8)
-     RegistrationMailer.new_user_with_invitation_email(email,generated_password).deliver
-     user = User.create!(email: email, password: generated_password)
-   end
+  def self.create_user(email)
+   generated_password = Devise.friendly_token.first(8)
+   RegistrationMailer.new_user_with_invitation_email(email,generated_password).deliver
+   user = User.create!(email: email, password: generated_password)
+  end
 end
